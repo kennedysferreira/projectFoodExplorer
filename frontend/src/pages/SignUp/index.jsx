b82d76c73718fa99c8be3eec11d381e949f060a5
kennedy <sferreira.kennedy@gmail.com>
2024-10-02 17:00:32 +0100

    create signUp page

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,66 +1,75 @@
-import { toast } from "react-toastify";
+import { useAuth } from "../../hooks/auth";
 import { Form } from "../../components/Forms";
 import { Button } from "../../components/Button";
 import { FoodExplorer } from "../../components/FoodExplorer";
 import { Container, Logo, Section, MakeLogin, MakeAccount } from "./style";
+import { toast } from "react-toastify";
 
 import { useState } from "react";
-import { useAuth } from "../../hooks/auth";
 import { useNavigate } from "react-router-dom";
 
-
-
-export function SignIn() {
+export function SignUp() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { createAccount } = useAuth();
 
   const navigate = useNavigate();
-  const { sessionLogin } = useAuth();
-
-  
-
-  function handleSessionLogin() {
-  
-    if (password.length < 6) {
-      toast.error("Senha ou e-mail inválido.");
+  async function handleSignUp() {
+    if (name.length < 3 || email.length < 6) {
+      toast.dark("Preencha os campos com informações válidas.");
       return;
     }
-    sessionLogin({ email, password });
+
+    if (password.length < 6) {
+      toast.dark("Senha inválida, verifique e tente novamente.");
+      return;
+    }
+
+    const create = await createAccount({ name, email, password });
+    if (create) navigate(-1);
   }
 
   return (
     <Container>
       <Logo>
         <FoodExplorer />
-        
       </Logo>
 
       <Section>
-        <MakeLogin>Faça Login</MakeLogin>
-        <FoodExplorer className="logo" />
+        <FoodExplorer />
+        <MakeLogin>Crie Sua Conta</MakeLogin>
 
         <div className="loginForm">
           <Form
-            value={email}
+            label={"Nome"}
+            placeholder="Digite seu nome"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <Form
             label={"Email"}
             placeholder="Exemplo: teste@exemplo.com"
             type="email"
             onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
 
           <Form
             label={"Senha"}
-            placeholder="No mínimo 6 caracteres"
+            placeholder="Digite sua senha"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-
-          <Button title="Entrar" onClick={handleSessionLogin} />
+          <Button title="Criar Conta" onClick={handleSignUp} />
         </div>
 
-        <MakeAccount onClick={() => navigate("/signUp")}>
-          Criar uma conta
+        <MakeAccount onClick={() => navigate(-1)}>
+          {" "}
+          Já possui uma conta?{" "}
         </MakeAccount>
       </Section>
     </Container>
---
+++ frontend/src/pages/SignUp/style.js
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  min-height: 100dvh;
+  max-width: 100%;
+
+  @media (min-width: ${({ theme }) => theme.MEDIA_QUERY.LARGE}) {
+    justify-content: space-around;
+    padding: 0 5%;
+  }
+
+  @media (min-width: ${({ theme }) => theme.MEDIA_QUERY.LARGE_G}) {
+    justify-content: center;
+    gap: 200px;
+  }
+`;
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 4rem 1.6rem;
+
+  .loginForm {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 3.2rem;
+    margin: 7.3rem 0 3.2rem 0;
+
+    @media (min-width: ${({ theme }) => theme.MEDIA_QUERY.SMALL_M}) {
+      max-width: 32rem;
+    }
+
+    @media (min-width: ${({ theme }) => theme.MEDIA_QUERY.LARGE}) {
+      margin: 3.2rem 0;
+      max-width: 100%;
+    }
+  }
+
+  @media (min-width: ${({ theme }) => theme.MEDIA_QUERY.MEDIUM}) {
+    max-width: 50rem;
+    padding: 6rem;
+    border-radius: 1.6rem;
+    background-color: ${({ theme }) => theme.COLORS.DARK_700};
+  }
+
+  @media (min-width: ${({ theme }) => theme.MEDIA_QUERY.LARGE}) {
+    > div:nth-child(1) {
+      display: none;
+    }
+  }
+`;
+
+export const MakeLogin = styled.p`
+  font-size: 4rem;
+  font-family: "Poppins";
+  display: none;
+
+  @media (min-width: ${({ theme }) => theme.MEDIA_QUERY.LARGE}) {
+    display: block;
+  }
+`;
+
+export const MakeAccount = styled.p`
+  cursor: pointer;
+  font-family: "Poppins";
+  font-size: 1.4rem;
+
+  &:hover {
+    color: ${({ theme }) => theme.COLORS.CAKE_200};
+  }
+`;
+
+export const Logo = styled.div`
+  display: none;
+
+  @media (min-width: ${({ theme }) => theme.MEDIA_QUERY.LARGE}) {
+    display: flex;
+    flex-direction: column;
+    gap: 4rem;
+    width: 30rem;
+  }
+`;
