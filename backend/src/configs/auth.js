09ae01daee8efae1ec8141cd4e7c96e44ada82c6
kennedy <sferreira.kennedy@gmail.com>
2024-10-03 16:42:15 +0100

    separe backend/front end in different  files

--- backend/src/configs/auth.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  jwt: {
-    secret: process.env.AUTH_SECRET || "default",
-    expiresIn: 60 * 60 * 15, //15 minutos
-  },
-};
--- backend/src/routes/sessions.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const SessionsCOntroller = require("../controllers/SessionsController");
-
-const sessionsCOntroller = new SessionsCOntroller();
-
-const sessionRoutes = Router();
-sessionRoutes.post("/", sessionsCOntroller.create);
-
-module.exports = sessionRoutes;
