25ac2cc126cdb5f62c5487d12089da4fb233ffd6
kennedy <sferreira.kennedy@gmail.com>
2024-10-03 17:05:34 +0100

    Update user.routes.jsx

--- src/routes/user.routes.jsx
+++ src/routes/user.routes.jsx
@@ -1,7 +1,7 @@
 import { Cart } from "../pages/Cart";
 import { Home } from "../pages/Home";
 import { About } from "../pages/About";
-import { Profile } from "../pages/Profile";
+
 import { Payment } from "../pages/Payment";
 import { Favorite } from "../pages/Favorite";
 import { PlateView } from "../pages/PlateView";
@@ -19,7 +19,7 @@ export function UserRoutes() {
       <Route path="/cart" element={<Cart />} />
       <Route path="/order-history" element={<OrderHistory />} />
       <Route path="/favorites" element={<Favorite />} />
-      <Route path="/profile" element={<Profile />} />
+     
       <Route path="/about" element={<About />} />
 
       <Route path="*" element={<Home />} />
